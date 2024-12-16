de57aa695903d70eca486f0dd56f3b2fa86b3a1f
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-12-17 04:33:23 +0600

    Getting Ready For ECS

--- Assets/_Root/Scripts/Game/DI/Interfaces/Gameplay/IItemCollectedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pancake.Game.Interfaces
-{
-    /// <summary>
-    /// Represents an event that is invoked when the player collects a collectable.
-    /// </summary>
-    public interface IItemCollectedEvent : IEvent
-    {
-    }
-}
--- Assets/_Root/Scripts/Game/DI/Interfaces/Gameplay/IJoystick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Pancake.Game.Interfaces
-{
-    public interface IJoystick
-    {
-        Vector2 Direction { get; }
-    }
-}
