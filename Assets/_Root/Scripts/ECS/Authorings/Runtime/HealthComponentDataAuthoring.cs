5d68f952cedf113a16848742c7ee316b88334251
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-12-19 01:34:11 +0600

    Going to start working on data and authoring

---
+++ Assets/_Root/Scripts/ECS/Authorings/Runtime/HealthComponentDataAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace _Root.Scripts.ECS.Authorings.Runtime
+{
+    public class HealthComponentDataAuthoring : MonoBehaviour
+    {
+        private class HealthComponentDataAuthoringBaker : Baker<HealthComponentDataAuthoring>
+        {
+            public override void Bake(HealthComponentDataAuthoring authoring)
+            {
+            }
+        }
+    }
+}
