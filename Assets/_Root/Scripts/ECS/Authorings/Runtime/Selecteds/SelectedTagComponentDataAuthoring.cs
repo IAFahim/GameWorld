b2670cead8d698e211d744e4a75743e89b4a6cb3
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-12-21 05:40:53 +0600

    WIP: Selection

---
+++ Assets/_Root/Scripts/ECS/Authorings/Runtime/Selecteds/SelectedTagComponentDataAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using _Root.Scripts.Tags.Runtime.Selecteds;
+using Unity.Entities;
+using UnityEngine;
+
+namespace _Root.Scripts.ECS.Authorings.Runtime.Selecteds
+{
+    public class SelectedTagComponentDataAuthoring : MonoBehaviour
+    {
+        public class SelectedTagComponentDataBaker : Baker<SelectedTagComponentDataAuthoring>
+        {
+            public override void Bake(SelectedTagComponentDataAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new SelectedTagComponentData());
+                SetComponentEnabled<SelectedTagComponentData>(entity, false);
+            }
+        }
+    }
+}
